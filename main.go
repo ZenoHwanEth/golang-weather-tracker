e7a6cdbab5695e1fdb100b9f3a94613735154531
ZenoHwanEth <hwan2221@hotmail.com>
2023-07-22 20:37:04 +0800

    adding the main function

--- main.go
+++ main.go
@@ -1,5 +1,56 @@
 package main
 
-func main() {
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"strings"
+)
 
+type apiConfigData struct {
+	OpenWeatherMapApiKey string `json:"OpenWeatherMapApiKey"`
+}
+
+type weatherData struct {
+	Name string `json:"Name"`
+	Main struct {
+		Kevin float64 `json:"temp"`
+	} `json:"main"`
+}
+
+func loadApiConfig(fileName string) (apiConfigData, error) {
+	bytes, err := ioutil.ReadFile(fileName)
+
+	if err != nil {
+		return apiConfigData{}, err
+	}
+
+	var c apiConfigData
+
+	err = json.Unmarshal(bytes, &c)
+
+	if err != nil {
+		return apiConfigData{}, err
+	}
+
+	return c, nil
+}
+
+func main() {
+	http.HandleFunc("/hello", hello)
+
+	http.HandleFunc("/weather/", func(w http.ResponseWriter, r *http.Request) {
+		city := strings.SplitN(r.URL.Path, "/", 3)[2]
+		data, err := query(city)
+
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(data)
+	})
+
+	http.ListenAndServe(":8080", nil)
 }
